f96e9dd2359c46a5721f6c10386d1235117d2645
huuphuc <tranhuuphuc599@gmail.com git config --global user.email tranhuuphuc599@gmail.com git config --global user.email tranhuuphuc599@gmail.com>
2021-10-14 23:32:16 +0700

    add note in chuong2/bai4.c

--- chuong 2/bai4.c
+++ chuong 2/bai4.c
@@ -4,9 +4,9 @@ int main(){
 	int a,b,c;
 	printf("nhap a: "); scanf("%d" ,&a);
 	printf("nhap b: "); scanf("%d" ,&b);
-	c = a;
-	a = b;
-	b = c;
+	c = a; // gán a vào biến tạm c, khi này c sẽ bằng a
+	a = b;	// gán a bằng b 
+	b = c;	// gán b = biến tạm c (tức bằng với a ban đầu)	
 	printf("a sau khi hoan doi la %d\n" ,a);
 	printf("b sau khi hoan doi la %d\n" ,b);
 	return 0;
